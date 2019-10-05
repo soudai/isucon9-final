129311fb29c13a5c148042dd190330817dfd6448
Ryuta Kamizono <kamipo@gmail.com>
2019-10-05 12:54:48 +0900

    Add indexes

--- webapp/sql/95_alter_table.sql
+++ webapp/sql/95_alter_table.sql
@@ -1,1 +1,6 @@
-ALTER TABLE train_timetable_master ADD KEY (`date`,`train_class`,`train_name`,`station`);
+ALTER TABLE `train_timetable_master` ADD KEY (`date`,`train_class`,`train_name`,`station`);
+ALTER TABLE `train_master` ADD KEY (`date`,`train_class`,`train_name`);
+ALTER TABLE `seat_master` ADD KEY (`train_class`,`car_number`,`seat_row`,`seat_column`);
+ALTER TABLE `station_master` ADD KEY (`name`);
+ALTER TABLE `fare_master` ADD KEY (`train_class`,`seat_class`,`start_date`);
+ALTER TABLE `distance_fare_master` ADD KEY (`distance`);
